398ee9b21d29d433088be2f19aecc81d8ac2cc76
DavidTorres-sys <david.torresg@udea.edu.co>
2021-11-27 15:08:32 -0500

    prueba punto 4

---
+++ src/Punto4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Punto4 {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int withdraw;
+    System.out.print("Ingrese el valor a retirar:");
+    withdraw = input.nextInt();
+
+    if(retirement == 1000) {
+      System.out.print("true");
+    }
+    else {
+      System.out.println("Dinero no disponible");
+    }
+  }
+}
