1c5e8e2d44945d108c19d079577bafe314129c63
DavidTorres-sys <david.torresg@udea.edu.co>
2021-11-28 14:48:04 -0500

    b

--- src/PuntoCuatro.java
+++ src/PuntoCuatro.java
@@ -6,38 +6,24 @@ public class PuntoCuatro {
     Scanner input = new Scanner(System.in);
 
     int withdraw;
+    int billeteCienMil;
+    int billeteCincuentaMil;
+    int billeteVeinteMil;
+    int billeteDiezMil;
+    int billeteCincoMil;
+    int billeteDosMil;
+    int billeteMil;
+    int billeteQuinientos;
+    int billeteDocientos;
+    int billeteCien;
+
     System.out.print("Ingrese el valor a retirar:");
     withdraw = input.nextInt();
 
-    for(int k = 1; k <= withdraw; k-- ){
-      int x =  withdraw - k;
+    for(int k = 1; k <= n; k-- ){
       if(withdraw >= 100000) {
-        int y = withdraw - k;
+        int y = withdraw - 100000;
         System.out.println(y);
-      }
-      if(withdraw <= 50000){
-        System.out.println();
-      }
-      if(withdraw <=  20000){
-        System.out.println();
-      }
-     if(withdraw <= 10000){
-        System.out.println();
-      }
-      if(withdraw <= 5000){
-        System.out.println();
-      }
-      if(withdraw <= 2000){
-        System.out.println();
-      }
-      if(withdraw <= 1000){
-        System.out.println();
-      }
-      if(withdraw <= 500){
-        System.out.println();
-      }
-      if(withdraw <= 100){
-        System.out.println();
       }
     }
   }
